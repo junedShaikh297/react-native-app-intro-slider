5f4557a5e00dcd84be7c68db2d0485da88d07bf2
junedShaikh297 <47630556+junedShaikh297@users.noreply.github.com>
2020-02-05 15:48:42 +0530

    Update AppIntroSlider.js
    dot click disabled and button click disabled. now you can render you button and change what you like.

--- AppIntroSlider.js
+++ AppIntroSlider.js
@@ -122,6 +122,7 @@ export default class AppIntroSlider extends React.Component {
     return (
       <View style={!this.props.bottomButton && style}>
         <TouchableOpacity
+          disabled={true}
           onPress={onPress}
           style={this.props.bottomButton ? styles.flexOne : this.props.buttonStyle}
         >
@@ -156,7 +157,8 @@ export default class AppIntroSlider extends React.Component {
         <View style={styles.paginationDots}>
           {this.props.slides.length > 1 &&
             this.props.slides.map((_, i) => (
-              <TouchableOpacity
+              <View
+                disabled={true}
                 key={i}
                 style={[
                   styles.dot,
@@ -164,7 +166,6 @@ export default class AppIntroSlider extends React.Component {
                     ? this.props.activeDotStyle
                     : this.props.dotStyle,
                 ]}
-                onPress={() => this._onPaginationPress(i)}
               />
             ))}
         </View>
